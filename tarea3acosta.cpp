8006215d7152e34485d402a3eac017078c7e60b7
JPAcosta4 <jpacosta@miuandes.cl>
2022-10-26 21:44:34 -0300

    pbb especialidad

--- tarea3acosta.cpp
+++ tarea3acosta.cpp
@@ -8,6 +8,14 @@ int main(int argc, char** argv){
 // propias clases herederas. 
 
 
+/*
+Para las PBB de las especialidades. 
+You can easily implement this using the rand function:
+
+bool TrueFalse = (rand() % 100) < 75;
+The rand() % 100 will give you a random number between 0 and 100, and the probability of it being under 75 is, well, 75%. You can substitute the 75 for any probability you want.
+*/
+
 
     return 0; 
 }
