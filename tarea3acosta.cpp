bbf07affb147cf7c6a239451c327e451d1a0eb13
JPAcosta4 <jpacosta@miuandes.cl>
2022-10-29 11:13:04 -0300

    probando

--- tarea3acosta.cpp
+++ tarea3acosta.cpp
@@ -16,6 +16,6 @@ bool TrueFalse = (rand() % 100) < 75;
 The rand() % 100 will give you a random number between 0 and 100, and the probability of it being under 75 is, well, 75%. You can substitute the 75 for any probability you want.
 */
 
-
+jhagdjhasgdhjgagdasjhdghajdg
     return 0; 
 }
